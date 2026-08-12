6e7c4562d0e8d6768ec860df9a5af6fb590bac37
omar <omarabuzaid587@gmail.com>
2026-08-12 18:46:13 -0400

    Add AppImage support and snprintf refactor for .deb branch

--- main.c
+++ main.c
@@ -30,12 +30,12 @@ if (strcmp(argv[1], "spotify") == 0) {
     printf("program name invalid: %s\n", argv[1]);
 }
 
-    size_t len = strlen(argv[1]);
-    if (len >= 4 &&
-        argv[1][len - 4] == '.' &&
-        argv[1][len - 3] == 'd' &&
-        argv[1][len - 2] == 'e' &&
-        argv[1][len - 1] == 'b') {
+    size_t len0 = strlen(argv[1]);
+    if (len0 >= 4 &&
+        argv[1][len0 - 4] == '.' &&
+        argv[1][len0 - 3] == 'd' &&
+        argv[1][len0 - 2] == 'e' &&
+        argv[1][len0 - 1] == 'b') {
         char *buffer = malloc(
             strlen("sudo apt install ") +
             strlen(argv[1]) +
@@ -53,17 +53,17 @@ if (strcmp(argv[1], "spotify") == 0) {
         free(buffer);
     }
 
-    size_t len = strlen(argv[1]);
+    size_t len1 = strlen(argv[1]);
     if (len >= 9 &&
-        argv[1][len - 9] == '.' &&
-        argv[1][len - 8] == 'A' &&
-        argv[1][len - 7] == 'p' &&
-        argv[1][len - 6] == 'p' &&
-        argv[1][len - 5] == 'I' &&
-        argv[1][len - 4] == 'm' &&
-        argv[1][len - 3] == 'a' &&
-        argv[1][len - 2] == 'g' &&
-        argv[1][len - 1] == 'e') {
+        argv[1][len1 - 9] == '.' &&
+        argv[1][len1 - 8] == 'A' &&
+        argv[1][len1 - 7] == 'p' &&
+        argv[1][len1 - 6] == 'p' &&
+        argv[1][len1 - 5] == 'I' &&
+        argv[1][len1 - 4] == 'm' &&
+        argv[1][len1 - 3] == 'a' &&
+        argv[1][len1 - 2] == 'g' &&
+        argv[1][len1 - 1] == 'e') {
         char buffer[256];
         snprintf(buffer, sizeof(buffer), "sudo chmod +x %s", argv[1]);
         system(buffer);
@@ -71,17 +71,37 @@ if (strcmp(argv[1], "spotify") == 0) {
     }
 
 
-    size_t len = strlen(argv[1]);
-    if (len > = 7 &&
-        argv[1][len - 7] == '.' &&
-        argv[1][len - 6] == 't' &&
-        argv[1][len - 5] == 'a' &&
-        argv[1][len - 4] == 'r' &&
-        argv[1][len - 3] == '.' &&
-        argv[1][len - 2] == 'g' &&
-        argv[1][len - 1] == 'z') {
-        char buffer[256];
-        snprintf(buffer, "")
-    }
+    size_t len2 = strlen(argv[1]);
+    if (len >= 7 &&
+        argv[1][len2 - 7] == '.' &&
+        argv[1][len2 - 6] == 't' &&
+        argv[1][len2 - 5] == 'a' &&
+        argv[1][len2 - 4] == 'r' &&
+        argv[1][len2 - 3] == '.' &&
+        argv[1][len2 - 2] == 'g' &&
+        argv[1][len2 - 1] == 'z') {
+
+     FILE *fp = fopen("Makefile", "r");
+     FILE *fp2 = fopen("configure", "r");
+
+        if (fp != NULL) {
+            printf("make file found\n");
+            char buffer[256];
+            snprintf(buffer, sizeof(buffer), "sudo make && sudo make install");
+            system(buffer);
+        }
+        else if (fp2 != NULL) {
+            printf("configure file found\n");
+            char buffer[256];
+            snprintf(buffer, sizeof(buffer), "sudo ./configure && sudo make && sudo make install");
+            system(buffer);
+        }
+        else {
+            printf("no make file or config file found");
+            return -1;
+        }
+     if (fp != NULL) fclose(fp);
+        if (fp2 != NULL) fclose(fp2);
+        }
     return 0;
 }
